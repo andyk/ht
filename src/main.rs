df09aa73c352fdcb2c4fa4469f2562a7ab5dc1b6
Marcin Kulik <m@ku1ik.com>
2024-04-02 10:40:01 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,19 @@ fn main() {
         match &json["action"].as_str() {
             Some("input") => {
                 // pty.input();
-            },
+            }
 
             Some("getView") => {
-                let text = vt.lines().iter().map(|l| l.text()).collect::<Vec<_>>().join("\n");
+                let text = vt
+                    .lines()
+                    .iter()
+                    .map(|l| l.text())
+                    .collect::<Vec<_>>()
+                    .join("\n");
+
                 let resp = serde_json::json!({ "view": text });
                 println!("{}", serde_json::to_string(&resp).unwrap());
-            },
+            }
 
             _ => (),
         }
