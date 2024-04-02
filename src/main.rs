fbc648595e1d5fafa6199bd9f8fc2badf072741f
Marcin Kulik <m@ku1ik.com>
2024-04-02 12:11:33 +0200

    Create Vt instance once

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
+    let mut vt = avt::Vt::builder().size(80, 24).build();
+
     for line in std::io::stdin().lines() {
-        let mut vt = avt::Vt::builder().size(80, 24).build();
         let json: serde_json::Value = serde_json::from_str(&line.unwrap()).unwrap();
 
         match &json["action"].as_str() {
