3b0b1c5bb495646afb3df3d03a346fc557581508
Marcin Kulik <m@ku1ik.com>
2024-04-02 13:55:11 +0200

    Launch bash in PTY

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+use std::{fs::File, io::Write, os::fd::{AsRawFd, FromRawFd, RawFd}};
+use nix::pty;
+use nix::unistd::{self, ForkResult};
+use nix::libc;
+use std::ffi::{CString, NulError};
+use std::io;
+
 fn main() {
+    let (rx, tx) = nix::unistd::pipe().unwrap();
+    let mut input = unsafe { File::from_raw_fd(tx.as_raw_fd()) };
+
+    let winsize = pty::Winsize {
+        ws_col: 80,
+        ws_row: 24,
+        ws_xpixel: 0,
+        ws_ypixel: 0,
+    };
+
+    let result = unsafe { pty::forkpty(Some(&winsize), None) }.unwrap();
+
+    match result.fork_result {
+        ForkResult::Parent { child } => handle_parent(result.master.as_raw_fd(), child, input),
+
+        ForkResult::Child => {
+            handle_child(&["/bin/bash"]).unwrap();
+            unreachable!();
+        }
+    }
+}
+
+fn handle_parent(master_fd: RawFd, child: unistd::Pid, mut input: File) {
     let mut vt = avt::Vt::builder().size(80, 24).build();
 
     for line in std::io::stdin().lines() {
@@ -6,7 +36,7 @@ fn main() {
 
         match json["action"].as_str() {
             Some("input") => {
-                // pty.input();
+                input.write_all(json["payload"].as_str().unwrap().as_bytes()).unwrap();
             }
 
             Some("getView") => {
@@ -24,4 +54,14 @@ fn main() {
             _ => (),
         }
     }
+}
+
+fn handle_child<S>(command: &[S]) -> io::Result<()> where S: AsRef<str> {
+    let command = command
+        .iter()
+        .map(|s| CString::new(s.as_ref()))
+        .collect::<Result<Vec<CString>, NulError>>().unwrap();
+
+    unistd::execvp(&command[0], &command).unwrap();
+    unsafe { libc::_exit(1) }
 }
