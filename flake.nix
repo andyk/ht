db9260cdef990f98f4e46ae77e88a836a040f7cf
Marcin Kulik <m@ku1ik.com>
2024-06-10 11:50:29 +0200

    Use Rust overlay for full reproducibility with nix flake

--- flake.nix
+++ flake.nix
@@ -2,26 +2,37 @@
   description = "ht";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              rustup
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs =
+            with pkgs;
+            [
+              (rust-bin.stable."1.74.0".default)
               bashInteractive
-            ] ++ (lib.optionals stdenv.isDarwin [
+            ]
+            ++ (lib.optionals stdenv.isDarwin [
               libiconv
               darwin.apple_sdk.frameworks.Foundation
             ]);
-
-            RUSTUP_TOOLCHAIN = "1.74";
-          };
-        }
-      );
+        };
+      }
+    );
 }
