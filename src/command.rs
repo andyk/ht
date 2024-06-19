05d74423d0469992f44130a2a8855cc4c54e7892
Marcin Kulik <m@ku1ik.com>
2024-06-19 10:39:13 +0200

    Add tests for command parsing

--- src/command.rs
+++ src/command.rs
@@ -48,3 +48,42 @@ where
 {
     serde_json::from_value(value).map_err(|e| e.to_string())
 }
+
+#[cfg(test)]
+mod test {
+    use super::parse;
+    use super::Command;
+
+    #[test]
+    fn parse_input() {
+        let command = parse(r#"{ "type": "input", "payload": "hello" }"#).unwrap();
+        assert!(matches!(command, Command::Input(input) if input == "hello"));
+    }
+
+    #[test]
+    fn parse_input_missing_args() {
+        parse(r#"{ "type": "input" }"#).expect_err("should fail");
+    }
+
+    #[test]
+    fn parse_resize() {
+        let command = parse(r#"{ "type": "resize", "cols": 80, "rows": 24 }"#).unwrap();
+        assert!(matches!(command, Command::Resize(80, 24)));
+    }
+
+    #[test]
+    fn parse_resize_missing_args() {
+        parse(r#"{ "type": "resize" }"#).expect_err("should fail");
+    }
+
+    #[test]
+    fn parse_get_view() {
+        let command = parse(r#"{ "type": "getView" }"#).unwrap();
+        assert!(matches!(command, Command::GetView));
+    }
+
+    #[test]
+    fn parse_invalid_json() {
+        parse("{").expect_err("should fail");
+    }
+}
