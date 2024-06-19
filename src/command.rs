131bd430352dbd4ba36a190db079061eed6e8b99
Marcin Kulik <m@ku1ik.com>
2024-06-19 10:13:21 +0200

    Use structs for automatic parsing of command args

--- src/command.rs
+++ src/command.rs
@@ -1,3 +1,5 @@
+use serde::{de::DeserializeOwned, Deserialize};
+
 #[derive(Debug)]
 pub enum Command {
     Input(String),
@@ -5,32 +7,44 @@ pub enum Command {
     Resize(usize, usize),
 }
 
+#[derive(Debug, Deserialize)]
+struct InputArgs {
+    payload: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct ResizeArgs {
+    cols: usize,
+    rows: usize,
+}
+
 pub fn parse(line: &str) -> Result<Command, String> {
     serde_json::from_str::<serde_json::Value>(line)
-        .map_err(|e| format!("JSON parse error: {e}"))
+        .map_err(|e| e.to_string())
         .and_then(build_command)
 }
 
-fn build_command(json: serde_json::Value) -> Result<Command, String> {
-    match json["type"].as_str() {
+fn build_command(value: serde_json::Value) -> Result<Command, String> {
+    match value["type"].as_str() {
         Some("input") => {
-            let payload = json["payload"]
-                .as_str()
-                .ok_or("payload missing".to_string())?
-                .to_string();
-
-            Ok(Command::Input(payload))
+            let args: InputArgs = args_from_json_value(value)?;
+            Ok(Command::Input(args.payload))
         }
 
         Some("resize") => {
-            let cols = json["cols"].as_u64().ok_or("cols missing".to_string())?;
-            let rows = json["rows"].as_u64().ok_or("rows missing".to_string())?;
-
-            Ok(Command::Resize(cols as usize, rows as usize))
+            let args: ResizeArgs = args_from_json_value(value)?;
+            Ok(Command::Resize(args.cols, args.rows))
         }
 
         Some("getView") => Ok(Command::GetView),
 
         other => Err(format!("invalid command type: {other:?}")),
     }
+}
+
+fn args_from_json_value<T>(value: serde_json::Value) -> Result<T, String>
+where
+    T: DeserializeOwned,
+{
+    serde_json::from_value(value).map_err(|e| e.to_string())
 }
